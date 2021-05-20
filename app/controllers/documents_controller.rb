98292d003eb66441d83de2cad6858deda70d5398
Devon Wolfkiel <devonwolf@tutanota.com>
2021-05-20 11:50:38 -0700

    Add delete route, link back to documents page

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -35,6 +35,13 @@ class DocumentsController < ApplicationController
     end
   end
 
+  def destroy
+    @document = Document.find(params[:id])
+    @document.destroy
+
+    redirect_to documents_path
+  end
+
   private
   def document_params
     params.require(:document).permit(:title, :body)
