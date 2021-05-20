21d5ccef05eba940fa59f1a42fd93a43e567a882
Devon Wolfkiel <devonwolf@tutanota.com>
2021-05-20 11:31:26 -0700

    Add new document route

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -6,5 +6,24 @@ class DocumentsController < ApplicationController
   def show
     @document = Document.find(params[:id])
   end
-  
+
+  def new
+    @document = Document.new
+  end
+
+  def create
+    @document = Document.new(document_params)
+
+    if @document.save
+      redirect_to @document
+    else
+      render :new
+    end
+  end
+
+  private
+  def document_params
+    params.require(:document).permit(:title, :body)
+  end
+
 end
--- app/models/document.rb
+++ app/models/document.rb
@@ -1,2 +1,4 @@
 class Document < ApplicationRecord
+	validates :title, presence: true
+	validates :body, presence: true, length: { minimum: 10 }
 end
