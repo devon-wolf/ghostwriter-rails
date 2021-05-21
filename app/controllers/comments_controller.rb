2853428eb36c11239a11c9fe8f235535397493f6
Devon Wolfkiel <devonwolf@tutanota.com>
2021-05-20 18:27:32 -0700

    Add comment deletion and cascading delete

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,8 +5,15 @@ class CommentsController < ApplicationController
 		redirect_to document_path(@document)
 	end
 
+	def destroy
+		@document = Document.find(params[:document_id])
+		@comment = @document.comments.find(params[:id])
+		@comment.destroy
+		redirect_to document_path(@document)
+	end
+
 	private
 		def comment_params
-			params.require(:comment).permit(:commenter, :body. :status)
+			params.require(:comment).permit(:commenter, :body, :status)
 		end
 end
